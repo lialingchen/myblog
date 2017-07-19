858634c7d93fa916265fb41cdb4f1e7f2a918df4
lialingchen <lialilingchen@gmail.com>
2017-07-19 16:44:23 +0800

    commit all

---
+++ db/migrate/20170719083404_posts.rb
@@ -0,0 +1,10 @@
+class Posts < ActiveRecord::Migration[5.1]
+  def change
+      create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.references :user, foreign_key: true
+      t.timestamps
+  end
+  end
+end
